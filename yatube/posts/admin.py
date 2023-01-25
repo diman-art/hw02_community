d1e58cb95b82662a64f838b9b93fcad3513ad691
Dmitry Goryachev <goriachev.mity@yandex.ru>
2023-01-25 16:26:31 +0300

    Codereviver3

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -15,7 +15,7 @@ class PostAdmin(admin.ModelAdmin):
     search_fields = ('text',)
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
-    
+
 
 class GroupAdmin(admin.ModelAdmin):
     list_display = (
@@ -24,7 +24,7 @@ class GroupAdmin(admin.ModelAdmin):
         'slug',
         'description',
     )
-     
+
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Group, GroupAdmin)
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200,verbose_name = 'Заглавие')
+    title = models.CharField(max_length=200, verbose_name='Заглавие')
     slug = models.SlugField(unique=True)
     description = models.TextField()
 
@@ -20,19 +20,22 @@ class Post(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
     group = models.ForeignKey(
         Group,
-        blank = True,
-        null = True,
-        on_delete = models.SET_NULL,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
         related_name='posts'
     )
     author = models.ForeignKey(
         User,
-        on_delete = models.CASCADE,
-        related_name = 'posts'
+        on_delete=models.CASCADE,
+        related_name='posts'
+
     )
+
     class Meta:
+
         ordering = ['-pub_date']
 
     def __str__(self):
+
         return self.text[:15]
-        
